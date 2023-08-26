d06bbf1c8840f8d884a35849c17642c288db6b89
ivy <unjust@gmail.com>
2023-08-26 12:49:02 -0500

    test dta

---
+++ test/data.js
@@ -0,0 +1,51 @@
+export const data = [
+  {
+    "id": 0,
+    "firstName": "Daenerys",
+    "lastName": "Targaryen",
+    "fullName": "Daenerys Targaryen",
+    "title": "Mother of Dragons",
+    "family": "House Targaryen",
+    "imageUrl": "https://thronesapi.com/assets/images/daenerys.jpg",
+    "born": "284 DC"
+  },
+  {
+    "id": 1,
+    "firstName": "Samwell",
+    "lastName": "Tarly",
+    "fullName": "Samwell Tarly",
+    "title": "Maester",
+    "family": "House Tarly",
+    "imageUrl": "https://thronesapi.com/assets/images/sam.jpg",
+    "born": "283 DC"
+  },
+  {
+    "id": 2,
+    "firstName": "Jon",
+    "lastName": "Snow",
+    "fullName": "Jon Snow",
+    "title": "King of the North",
+    "family": "House Stark",
+    "imageUrl": "https://thronesapi.com/assets/images/jon-snow.jpg",
+    "born": "283 DC"
+  },
+  {
+    "id": 3,
+    "firstName": "Arya",
+    "lastName": "Stark",
+    "fullName": "Arya Stark",
+    "title": "No One",
+    "family": "House Stark",
+    "imageUrl": "https://thronesapi.com/assets/images/arya-stark.jpg",
+    "born": "289 AL"
+  },
+  {
+    "id": 4,
+    "firstName": "Sansa",
+    "lastName": "Stark",
+    "fullName": "Sansa Stark",
+    "title": "Lady of Winterfell",
+    "family": "House Stark",
+    "imageUrl": "https://thronesapi.com/assets/images/sansa-stark.jpeg",
+    "born": "286 AL"
+  }];
